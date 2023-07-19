ec57760ad7e1c23153c286cb5e5392a92667a500
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2023-07-19 21:43:33 +0300

    Create multiplestrings.py
    Leetcode #43

---
+++ multiplestrings.py
@@ -0,0 +1,3 @@
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(eval(num1 + '*' + num2))
+        ## Note to self. This is the simplest solution. There is the 'correct' way to solve it. Do that later!
