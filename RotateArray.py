06bc980501d5ce729596f010f9fd3dba1a596d83
SimonDerejeW <134320950+SimonDerejeW@users.noreply.github.com>
2023-08-09 12:22:37 +0300

    Create RotateArray.py
    Leetcode #189

---
+++ RotateArray.py
@@ -0,0 +1,8 @@
+def rotate(self, nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    i=0
+    while i < k:
+        nums.insert(0,nums.pop())
+        i+=1
